cea350c2ef854118fcc942c18fa2bc7595b69b67
Ja-baka <73071447+rapushka@users.noreply.github.com>
2022-05-26 12:29:09 +0300

    Simple MV

--- Assets/ProgramerSImulator/Scripts/User.cs
+++ Assets/ProgramerSImulator/Scripts/User.cs
@@ -1,36 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class User : MonoBehaviour
+public class User : IDisposable
 {
-    [SerializeField] private Timer _timer;
-    [SerializeField] private TMPro.TextMeshProUGUI _date;
+    private Timer _timer;
 
-    private int _expirience;
-    private int _money;
-    private System.DateTime _currentDate;
-
-    public int Expirience => _expirience;
-    public int Money => _money;
-
-    private void Start()
+    public User(Timer timer)
     {
-        _currentDate = new System.DateTime(2_000, 1, 1);
-    }
+        _timer = timer;
+        _currentDate = new DateTime(2_000, 1, 1);
 
-    private void OnEnable()
-    {
         _timer.Tick += OnTick;
     }
-    private void OnDisable()
+    public void Dispose()
     {
         _timer.Tick -= OnTick;
     }
 
+    private int _expirience;
+    private int _money;
+    private DateTime _currentDate;
+
+    public event Action Updated;
+
+    public string Expirience => _expirience.ToString();
+    public string Money => _money.ToString();
+    public string CurrentDate => _currentDate.ToShortDateString();
+
     private void OnTick()
     {
         _currentDate = _currentDate.AddDays(1);
-        _date.text = _currentDate.ToShortDateString();
+        Updated?.Invoke();
     }
 }
---
+++ Assets/ProgramerSImulator/Scripts/UserView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class UserView : MonoBehaviour
+{
+    [SerializeField] private Timer _timer;
+    [SerializeField] private TMPro.TextMeshProUGUI _date;
+
+    private User _userModel;
+
+    private void Start()
+    {
+        _userModel = new User(_timer);
+
+        _userModel.Updated += OnModelUpdated;
+    }
+
+    private void OnModelUpdated()
+    {
+        _date.text = _userModel.CurrentDate; 
+    }
+}
